0c46c6a4003c52b49847abe1627551e0210ec0c4
Sezhian-7 <sezhian@mindwaveventures.com>
2023-07-24 14:50:56 +0530

    statistic charts

---
+++ JsonData/statisticsData.tsx
@@ -0,0 +1,71 @@
+export const statisticList = [
+    {
+        category: 'Food',
+        amount: '₹ 8,000',
+        iconName: 'food-turkey',
+        type: 'income',
+        iconColor: '#845FD1',
+        percentage: '20%'
+    },
+    {
+        category: 'Travel',
+        amount: '₹ 3,000',
+        iconName: 'airplane-takeoff',
+        type: 'income',
+        iconColor: '#EF731B',
+        percentage: '30%'
+    },
+    {
+        category: 'Grocery',
+        amount: '₹ 3,500',
+        iconName: 'cart-outline',
+        type: 'income',
+        iconColor: '#31A0D7',
+        percentage: '30%'
+    },
+    {
+        category: 'Bills',
+        amount: '₹ 1,700',
+        iconName: 'file-edit-outline',
+        type: 'income',
+        iconColor: '#F4CB21',
+        percentage: '20%'
+    },
+   
+] 
+
+export const incomeStatisticList = [
+    {
+        category: 'Salary',
+        amount: '₹ 30,000',
+        iconName: 'currency-inr',
+        type: 'income',
+        iconColor: '#34BF03',
+        percentage: '20%'
+    },
+    {
+        category: 'House rent',
+        amount: '₹ 20,000',
+        iconName: 'home',
+        type: 'income',
+        iconColor: '#2A96A2',
+        percentage: '30%'
+    },
+    {
+        category: 'Car rent',
+        amount: '₹ 50,000',
+        iconName: 'car',
+        type: 'income',
+        iconColor: '#16BAC5',
+        percentage: '30%'
+    },
+    {
+        category: 'Music class',
+        amount: '₹ 40,000',
+        iconName: 'playlist-music',
+        type: 'income',
+        iconColor: '#9948A6',
+        percentage: '20%'
+    },
+   
+] 
--- Pages/Statistics.tsx
+++ Pages/Statistics.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BackgroundLayout from '../Layout/BackgroundLayout';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { VictoryPie } from "victory-native";
 import { Dimensions } from "react-native";
+import ExpensesContent from '../Components/ExpensesContent';
+import ExpensesTab from '../Components/ExpensesTab';
+import TextInputField from '../Components/TextInputField';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
+import moment from 'moment';
+import Card from '../Components/Card';
+import { statisticList, incomeStatisticList } from '../JsonData/statisticsData';
+
 
 const { width, height } = Dimensions.get("window");
 
@@ -10,39 +18,111 @@ interface StatisticsProps {
 
 }
 
-export const Statistics: React.FC<StatisticsProps> = ({ }) => {
+export const Statistics: React.FC<StatisticsProps> = ({ navigation }: any) => {
+    const [selected, setSelected] = useState('');
+    const monthAndDate = `${moment(selected || new Date()).format('MMMM')} ${moment(selected || new Date()).format('YYYY')}`
     const data = [
         { quarter: 1, earnings: 13000 },
         { quarter: 2, earnings: 16500 },
         { quarter: 3, earnings: 14250 },
         { quarter: 4, earnings: 19000 }
     ];
+    const [formType, setFormType] = useState("Statistics");
+    const onBackArrowPress = () => {
+        navigation.goBack()
+    }
+    const [isIncome, setIncome] = useState(false);
+    const onPressExpense = () => {
+        setIncome(false)
+    }
+    const onPressIncome = () => {
+        setIncome(true)
+    }
+    const handlePrev = () => {
+        let month = moment(selected || new Date()).subtract(1, 'months').format('YYYY-MM-DD');
+        setSelected(month);
+    }
+
+    const handleNext = () => {
+        let month = moment(selected || new Date()).add(1, 'months').format('YYYY-MM-DD');
+        setSelected(month);
+    }
+
+    const expenseColors = ["#31A0D7", "#845FD1", "#EF731B", "#F4CB21"]
+
+    const incomeColors = ["#34BF03", "#9948A6", "#2A96A2", "#16BAC5"]
+
     return (
         <BackgroundLayout>
             <>
                 <ScrollView style={{ flex: 1 }}>
                     <View style={styles.statBlk}>
-                        <Text>Statistics Page</Text>
-                        <VictoryPie
-                            width={350}
-                            height={400}                           
-                            colorScale={["#31A0D7", "#845FD1", "#EF731B", "#2A96A2", "F4CB21"]}
-                            innerRadius={70}
-                            labelRadius={({ innerRadius }: any) => (width * 0.39 + innerRadius) / 2.5}
-                            padAngle={2}
-                            style={{
-                                labels: {
-                                    fontSize: 16,
-                                    fill: '#fff',
-                                }
-                            }}
-                            data={[
-                                { x: "Cats", y: 40, label: '40%' },
-                                { x: "Dogs", y: 20, label: '20%' },
-                                { x: "Birds", y: 24, label: '24%' },
-                                { x: "Ducks", y: 16, label: '16%' },
-                            ]}
+                        <ExpensesContent
+                            screenTitle={`${formType}`}
+                            onBackArrowPress={onBackArrowPress}
                         />
+                        <ExpensesTab
+                            onPressExpense={onPressExpense}
+                            onPressIncome={onPressIncome}
+                            isIncome={isIncome}
+                        />
+                        <View style={styles.formBlk}>
+                            <View style={styles.dateBlk}>
+                                <Pressable onPress={handlePrev}>
+                                    <MIcon name="chevron-left" size={32} color="#000" />
+                                </Pressable>
+                                <TextInputField addStyle={{ fontSize: 18 }} content={monthAndDate} />
+                                <Pressable onPress={handleNext}>
+                                    <MIcon name="chevron-right" size={32} color="#000" />
+                                </Pressable>
+                            </View>
+                            <VictoryPie
+                                width={350}
+                                height={350}
+                                colorScale={isIncome ? incomeColors : expenseColors}
+                                innerRadius={70}
+                                labelRadius={({ innerRadius }: any) => (width * 0.39 + innerRadius) / 2.5}
+                                padAngle={2}
+                                style={{
+                                    labels: {
+                                        fontSize: 16,
+                                        fill: '#fff',
+                                    }
+                                }}
+                                data={[
+                                    { x: "Cats", y: 40, label: '40%' },
+                                    { x: "Dogs", y: 20, label: '20%' },
+                                    { x: "Birds", y: 24, label: '24%' },
+                                    { x: "Ducks", y: 16, label: '16%' },
+                                ]}
+                            />
+
+                            {!isIncome ? <>
+                                {statisticList.map((card, index) => {
+                                    return <Card
+                                        key={index}
+                                        category={card.category}
+                                        amount={card.amount}
+                                        iconName={card.iconName}
+                                        type={card.type}
+                                        iconColor={card.iconColor}
+                                    />
+                                })}
+                            </> : <>
+                                {incomeStatisticList.map((card, index) => {
+                                    return <Card
+                                        key={index}
+                                        category={card.category}
+                                        amount={card.amount}
+                                        iconName={card.iconName}
+                                        type={card.type}
+                                        iconColor={card.iconColor}
+                                    />
+                                })}
+                            </>}
+
+                        </View>
+
                     </View>
 
                 </ScrollView>
@@ -65,6 +145,15 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center'
+    },
+    formBlk: {
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        padding: 20,
+        shadowOffset: { width: 1, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        elevation: 3,
     },
 
 })
